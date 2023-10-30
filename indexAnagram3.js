a0f5fdd82dc16ac35b1e84bb1ec73de2a283d8d5
ugendar143 <146059449+ugendar143@users.noreply.github.com>
2023-10-30 13:14:14 +0530

    Add files via upload

---
+++ indexAnagram3.js
@@ -0,0 +1,13 @@
+let n=45612
+let narr[0,0,0,0,0,0,0,0,0,0]
+let nr;
+while (n!=0){
+    nr=n%10;
+    n=n/10;
+    n=|0;
+    narr[nr]++
+}
+let is Anagram=true
+for(let i=0;i<=9;i++){
+    for let
+}
