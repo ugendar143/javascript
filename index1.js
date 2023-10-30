68074aebb567cb8f346b0b2f5ee00f4a19a0de7b
ugendar143 <146059449+ugendar143@users.noreply.github.com>
2023-10-30 13:10:25 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        let a=500;
+        let b=300;
+        let c=23;
+        let max,min
+        if (a>b){
+           max=a
+           min=b
+        }
+        else if{
+        max=b
+        min=c
+        }
+        else{
+            max=c
+            min=a
+        }
+        console
+    </head>
+</html>
